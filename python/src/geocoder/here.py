0761f554e0c5ad8c910c302f1c4500fd5476adf4
Dmitriy Pleshcheev <dimychspb7@mail.ru>
2021-03-30 14:58:02 +0300

    async geocoder

--- python/src/geocoder/here.py
+++ python/src/geocoder/here.py
@@ -1,6 +1,9 @@
+from asyncio import get_event_loop
 from typing import Optional
 
+import orjson
 import requests
+from aiohttp import ClientSession
 from loguru import logger
 
 
@@ -30,6 +33,36 @@ class HereGeocoder:
         r = requests.get(self.__reverse, params=params)
         logger.info(f'Here API, get from point, request: {r.status_code}, {r.reason}')
         return r.json()
+
+    def batch_geocode(self, addresses: list[str]) -> dict[str, tuple[float, float]]:
+        async def runner():
+            async with ClientSession() as session:
+                return await self.download(session, addresses)
+
+        return get_event_loop().run_until_complete(runner())
+
+    async def download(self, session, addresses: list[str], batch=300) -> dict[str, Optional[tuple[float, float]]]:
+        address_set = list(set(addresses))
+        size = len(address_set)
+        result = {}
+
+        for start in range(0, size, batch):
+            addr_slice = address_set[start: start + batch]
+            result |= {addr: await self.__geocode(session, addr) for addr in addr_slice}
+
+        return result
+
+    async def __geocode(self, session, addr: str, lang: str = 'ru-RU') -> Optional[tuple[float, float]]:
+        params = {'q': addr, 'lang': lang, 'countryCode': 'RUS', 'apiKey': self.__api_key}
+        result = await self.__get(self.__geocoder, session, params)
+        return self.get_point(result)
+
+    @staticmethod
+    async def __get(url: str, session: ClientSession, params: dict) -> dict:
+        async with session.get(url, params=params) as resp:
+            if resp.status != 200:
+                raise Exception(f'Response error: {resp.status} - {resp.reason}')
+            return orjson.loads(await resp.text('utf-8'))
 
     @staticmethod
     def get_point(result: dict) -> Optional[tuple[float, float]]:
